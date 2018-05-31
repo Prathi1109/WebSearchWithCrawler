76cd6089ce395fed1c386a6b7b26af070fad726e
hazelcharm <39805745+hazelcharm@users.noreply.github.com>
2018-05-31 20:19:53 +0200

    Update ExecutionModule.java

--- ExecutionModule.java
+++ ExecutionModule.java
@@ -1,10 +1,3 @@
-package com.ir.programming.task2;
-
-import java.util.List;
-import java.util.ArrayList;
-
-public class ExecutionModule {
-	
    /*
 	* Author: Srilakshmi Sruthi Pasumarthy
 	* This class includes the invocation of all the methods to implement Crawler, Normalizing URLs, Indexing crawled pages, 
@@ -15,6 +8,11 @@ public class ExecutionModule {
 	* 'query' : of type String- search query;
 	* 'crawledURLs' : of type 'CrawledURL' -an ArrayList of custom datatype to access the crawled URLs along with their depths
 	*/
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ExecutionModule {
 
 	private static String seedURL;
 	private static int crawlDepth;
@@ -24,42 +22,29 @@ public class ExecutionModule {
 	
 	public static void main(String[] args) {
 		
-		if(args.length < 4)
-		{
-			System.out.println("Please check the inputs given. Inputs are to be given in the following order--");
-			System.out.println("1.Seed URL; 2.Crawl Depth; 3.Path where index files are to be stored; 4.Query");
-		}
-		else
-		{
-			seedURL = args[0];
-			crawlDepth = Integer.parseInt(args[1]);
-			indexPath = args[2];
-			if(args.length == 4)
-			{
-				query = args[3];
-			}
-			else if(args.length > 4)
-			{
-				for(int i=3; i<args.length; i++)
-				{
-					query += args[i]+" ";
-				}
-			}
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter the seed URL: " );
+		seedURL = scanner.next();
+		System.out.println("Please enter the crawl depth: " );
+		crawlDepth = scanner.nextInt();
+		System.out.println("Please enter the path where index files are to be stored: " );
+		indexPath = scanner.next();
+		System.out.println("Please enter the search query: " );
+		query = scanner.next();
 		
-			Crawler crawler = new Crawler();
-			crawledURLs = crawler.getCrawledLinks(seedURL, crawlDepth);
-			System.out.println("Crawling functionality has been applied successfully!");
-			NormalizationModule optimizer = new NormalizationModule();
-			crawledURLs = optimizer.normalize(indexPath, crawledURLs);
-			System.out.println("URL Normalization functionality has been applied successfully!");
-			IndexModule indexer = new IndexModule();
-			indexer.performIndexingAndStemming(indexPath, crawledURLs);
-			SearchModule searchOperation = new SearchModule();
-			searchOperation.search(indexPath, query);
-			System.out.println("Search Implementation has been applied successfully!");
-			RankModule rankOperation = new RankModule();
-			rankOperation.ranking(indexPath, query);
+		Crawler crawler = new Crawler();
+		crawledURLs = crawler.getCrawledLinks(seedURL, crawlDepth);
+		System.out.println("Crawling functionality has been applied successfully!");
+		NormalizationModule optimizer = new NormalizationModule();
+		crawledURLs = optimizer.normalize(indexPath, crawledURLs);
+		System.out.println("URL Normalization functionality has been applied successfully!");
+		IndexModule indexer = new IndexModule();
+		indexer.performIndexingAndStemming(indexPath, crawledURLs);
+		SearchModule searchOperation = new SearchModule();
+		searchOperation.search(indexPath, query);
+		System.out.println("Search Implementation has been applied successfully!");
+		RankModule rankOperation = new RankModule();
+		rankOperation.ranking(indexPath, query);
 		}
 
 	}
